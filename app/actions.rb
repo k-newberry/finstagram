e7498c68ffc93e10f5cecb340c60ac912941574c
k-newberry <knewberry@develop.bc.ca>
2020-11-06 03:17:37 +0000

    changes for deployment

--- app/actions.rb
+++ app/actions.rb
@@ -1,73 +1,103 @@
-# get '/' do
-#   File.read(File.join('app/views', 'index.html'))
-# end
-
-
-def humanized_time_ago(time_ago_in_minutes)
-    if time_ago_in_minutes >= 60
-        "#{time_ago_in_minutes/60} hours ago"
-    else
-        "#{time_ago_in_minutes} minutes ago"
+helpers do
+    def current_user
+        User.find_by(id: session[:user_id])
     end
 end
 
 get '/' do
-    @finstagram_post_moon = {
-        username: "Moon_Moon",
-        avatar_url: "https://i.pinimg.com/originals/75/d2/47/75d24724e13f083bce2c77541606f15b.png",
-        photo_url: "https://i.pinimg.com/originals/38/a7/50/38a750aa65fd48b39f31695ab416e20e.jpg",
-        humanized_time_ago: humanized_time_ago(15),
-        like_count: 4,
-        comment_count: 2,
-        comments: [{
-            username: "Moon_Moon",
-            text: "Moon_Moon: Moon Moon can't resize photos...silly Moon Moon!"
-         },
-        {
-            username: "MajesticAF_wolf",
-            text: "This is why we can't have nice things."
-        }]
-    }
-
-    @finstagram_post_pack = {
-        username: "wolf_pack_1",
-        avatar_url: "https://cdn.hswstatic.com/gif/pack-mentality-1.jpg",
-        photo_url: "https://i.pinimg.com/564x/b4/27/aa/b427aa32ef60dd2676dca34f06d3dbef.jpg",
-        humanized_time_ago: humanized_time_ago(65),
-        like_count: 2,
-        comment_count: 3,
-        comments: [{
-            username: "wolf_pack_1",
-            text: "#familyphotoswithMoonMoon"
-        },
-        {
-            username: "Moon_Moon",
-            text: "#beautifulselfies"
-        },
-        {
-            username: "MajesticAF_wolf",
-            text: "Zoom is fine, don't drag the pack into your madness."
-        }]
-    }
-
-    @finstagram_post_majestic = {
-        username: "MajesticAF_wolf",
-        avatar_url: "https://external-preview.redd.it/JlvjfDbBt8I5rrR-RscthLmkINwfi4NcZJVrb6g2ywI.jpg?auto=webp&s=571bb793cf63fd8698f758c2bd8f623b83d7e1e6",
-        photo_url: "https://i.pinimg.com/236x/f3/d5/65/f3d56517e65cec79a414755962ff1601.jpg",   
-        humanized_time_ago: humanized_time_ago(190),
-        like_count: 10,
-        comment_count: 2,
-        comments: [{
-            username: "MajesticAF_wolf",
-            text: "Moon Moon has nothing on me!"
-        },
-        {
-            username: "Moon_Moon",
-            text: "Mom says I'm beautiful."
-        }]
-    }
-    @finstagram_posts = [@finstagram_post_moon, @finstagram_post_pack, @finstagram_post_majestic]
-erb(:index)
+    @finstagram_posts = FinstagramPost.order(created_at: :desc)
+    erb(:index)
 end
 
+get '/signup' do
+    @user = User.new
+    erb(:signup)
+end
 
+post '/signup' do
+   
+    email = params[:email]
+    avatar_url = params[:avatar_url]
+    username = params[:username]
+    password = params[:password]
+
+    @user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password })
+    
+    if @user.save
+        redirect to ('/login')
+    else
+        erb(:signup)
+    end
+end
+
+get '/login' do
+    erb(:login)
+end
+    
+
+
+post '/login' do
+    username = params[:username]
+    password = params[:password]
+
+   user = User.find_by(username: username)
+
+    if user && user.password == password
+        session[:user_id] = user.id
+        redirect to('/')
+     else
+       @error_message = "Womp Womp! Login failed."
+       erb(:login)
+     end
+end
+
+get '/logout' do
+    session[:user_id] = nil
+    redirect to('/')
+end
+
+get '/finstagram_posts/new' do
+    @finstagram_post = FinstagramPost.new
+    erb(:"finstagram_posts/new")
+end
+
+post '/finstagram_posts' do
+    photo_url = params[:photo_url]
+    @finstagram_post = FinstagramPost.new({ photo_url: photo_url, user_id: current_user.id })
+
+    if @finstagram_post.save
+        redirect(to('/'))
+    else
+        erb(:"finstagram_posts/new")
+    end
+end
+
+get '/finstagram_posts/:id' do
+    @finstagram_post = FinstagramPost.find(params[:id])
+    erb(:"finstagram_posts/show")
+end
+
+post '/comments' do
+    text = params[:text]
+    finstagram_post_id = params[:finstagram_post_id]
+
+    comment = Comment.new({ text: text, finstagram_post_id: finstagram_post_id, user_id: current_user.id })
+    comment.save
+
+    redirect(back)
+end
+
+post '/likes' do    
+    finstagram_post_id = params[:finstagram_post_id]
+
+    like = Like.new({ finstagram_post_id: finstagram_post_id, user_id: current_user.id })
+    like.save
+
+    redirect(back)
+end
+
+delete '/likes/:id' do
+    like = Like.find(params[:id])
+    like.destroy
+    redirect(back)
+end
